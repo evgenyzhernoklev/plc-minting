109a436225f3f70c91b8ae51cedec14c5f781691
evgenyzhernoklev <oldschool127@mail.ru>
2018-04-26 14:59:59 +0300

    Add fixes

--- src/js/viewer.js
+++ src/js/viewer.js
@@ -1,6 +1,9 @@
 var Viewer = function() {
   this.window = $(window);
   this.body = $('body');
+  this.fullHeightElements = $('.js-full-height');
+  this.scrollLinks = $('.js-scroll-link');
+  this.menu = $('.js-menu-container');
   this.init();
 };
 
@@ -8,6 +11,7 @@ Viewer.prototype.init = function () {
   this.initClasses();
   this.initFunctions();
   this.window.on('resize', this.updateResizer.bind(this));
+  this.scrollLinks.on('click', this.scrollToBlock.bind(this));
 };
 
 Viewer.prototype.initClasses = function () {
@@ -21,8 +25,25 @@ Viewer.prototype.initClasses = function () {
 
 Viewer.prototype.initFunctions = function () {
   $(window).enllax();
+  this.fullWindowHeight();
 };
 
 Viewer.prototype.updateResizer = function () {
+  this.fullWindowHeight();
+};
 
+Viewer.prototype.fullWindowHeight = function () {
+  var newHeight = this.window.height();
+
+  this.fullHeightElements.height(newHeight);
+};
+
+Viewer.prototype.scrollToBlock = function (e) {
+  e.preventDefault();
+  var target = $(e.target).closest('.js-scroll-link').attr('href'),
+      targetPositionTop = $(target).offset().top - this.menu.height() * 1.5;
+
+  $('body, html').animate({
+    scrollTop: targetPositionTop
+  }, 700);
 };
